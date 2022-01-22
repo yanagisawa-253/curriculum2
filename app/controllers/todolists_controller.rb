c245ddcb21c756aea1217a62b773d7bdd9f6c6c9
yanagisawa-253 <anaguma253@gmail.com>
2022-01-22 13:47:20 +0000

    [update]編集機能

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -15,7 +15,17 @@ class TodolistsController < ApplicationController
   def create
     @list = List.new(list_params)
     @list.save
-    redirect_to root_path
+    redirect_to todolist_path(list.id)
+  end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to todolist_path(@list.id)
   end
   
   private
