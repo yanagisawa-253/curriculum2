6f02916268c2584c51b20942c99350090f7be112
yanagisawa-253 <anaguma253@gmail.com>
2022-01-22 14:03:50 +0000

    [update]初回画像投稿のエラー解決

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '31d530baf4533a9ea670c1e93da44ba6293449ef73e158165549e7880638e81011bc0277b517d28e8d5fae53f7f5eef2e10217da4cdbfa59c50716f19d710906'
