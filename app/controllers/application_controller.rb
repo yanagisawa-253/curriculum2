e87bc7cca2299c7de9795e036a47948d73748e61
yanagisawa-253 <anaguma253@gmail.com>
2022-01-30 01:11:38 +0000

    [update]user_image_id追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-    # ログインユーザーのみtop,about以外の画面を開ける設定
-    before_action :authenticate_user!,except: [:top]
-    
-    before_action :configure_permitted_parameters, if: :devise_controller?
+# ログインユーザーのみtop,about以外の画面を開ける設定
+  # before_action :authenticate_user!,except: [:top, :about]
 
-　# ログイン後の設定
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+# ログイン後の設定
   def after_sign_in_path_for(resources)
       flash[:notice] = "Welcome! You have signed in successfully."
       user_path(current_user.id)
---
+++ db/migrate/20220130011104_add_user_image_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddUserImageIdToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :user_image_id, :string
+  end
+end
