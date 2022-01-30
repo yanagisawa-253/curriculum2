d1cd7d5ac72e1e0766c63f2f8ca071c042a9f323
yanagisawa-253 <anaguma253@gmail.com>
2022-01-30 00:43:37 +0000

    [update]ログイン、後の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,32 @@
 class ApplicationController < ActionController::Base
     # ログインユーザーのみtop,about以外の画面を開ける設定
     before_action :authenticate_user!,except: [:top]
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+　# ログイン後の設定
+  def after_sign_in_path_for(resources)
+      flash[:notice] = "Welcome! You have signed in successfully."
+      user_path(current_user.id)
+  end
+
+  def after_sign_up_path_for(resources)
+      flash[:notice] = "Welcome! You have signed up successfully."
+      user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resources)
+      flash[:notice] = "Signed out successfully."
+      root_path
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    if resource_class == User
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+       devise_parameter_sanitizer.permit(:account_update, keys: [:name,:email])
+    end
+  end
 end
