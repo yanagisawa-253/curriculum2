97fba38d9fee5314f8cbd8fc0fdc3768ac2f9629
yanagisawa-253 <anaguma253@gmail.com>
2022-02-01 04:06:05 +0000

    [update]admin/homes root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'homes/about' => 'homes#about'
+  
+  namespase :admin do
+      get 'admin/homes/top' => 'admin/homes#top'
+      resources :items
+    end
 end
