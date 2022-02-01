2e40c47881c9e66489b82460d2ecf3d9468ce425
yanagisawa-253 <anaguma253@gmail.com>
2022-02-01 06:31:33 +0000

    [update]top.htnl->top.htmlに名所変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'homes/about' => 'homes#about'
   
-  namespase :admin do
-      get 'admin/homes/top' => 'admin/homes#top'
-      resources :items
-    end
+  namespace :admin do
+    get 'admin/homes/top' => 'admin/homes#top'
+    resources :items
+  end
 end
