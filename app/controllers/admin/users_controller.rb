ae946c648ce45f44f6e6cff7bdbc87e558e26e22
yanagisawa-253 <anaguma253@gmail.com>
2022-02-02 11:40:57 +0000

    [update]users.controller記述

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,29 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end 
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render 'index'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
+    end
+  
 end
