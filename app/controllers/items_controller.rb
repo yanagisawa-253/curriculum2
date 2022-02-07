734a0f32fc4835fe03dd7e6208fe0976ff7e104c
yanagisawa-253 <anaguma253@gmail.com>
2022-02-07 02:05:14 +0000

    [update]controller記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,15 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
   
+  def show
+    @item = item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active)
+  end
 end
