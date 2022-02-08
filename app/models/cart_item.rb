7412310afff10cb19653bd3dd36797765daba2b7
yanagisawa-253 <anaguma253@gmail.com>
2022-02-08 09:28:02 +0000

    [update]cart_item.model変更

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,11 @@
 class CartItem < ApplicationRecord
   
+  belongs_to :item
+  belongs_to :user
+  
+  validates :item_id, :amount, presence: true
+  validates :amount, numericality:{ only_integer: true }
+  
   def sum_of_price
     item.with_tax_price * amount
   end 
