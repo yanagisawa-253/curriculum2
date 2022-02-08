e8982632e5435c8b4fddcac04812c08798107d54
yanagisawa-253 <anaguma253@gmail.com>
2022-02-08 09:21:53 +0000

    [update]カート機能追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,6 @@
 class CartItem < ApplicationRecord
+  
+  def sum_of_price
+    item.with_tax_price * amount
+  end 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   
   attachment :image
-  
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
