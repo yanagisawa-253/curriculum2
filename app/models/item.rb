49a78cb7697f641ee536bfa92c84ed1729b1c4a8
yanagisawa-253 <anaguma253@gmail.com>
2022-02-08 09:27:00 +0000

    [update]item.model変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   
+  belongs_to :genre
+  
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items, dependent: :destroy
+  
   attachment :image
+  
   def with_tax_price
     (price * 1.1).floor
   end
