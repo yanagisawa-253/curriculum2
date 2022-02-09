de3f2aa1ffa69ae8a543f271d73f3936504f80bb
yanagisawa-253 <anaguma253@gmail.com>
2022-02-09 06:55:50 +0000

    [update]user.controller作成

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,39 @@
 class UserController < ApplicationController
+    
+  def show
+    @customer =Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer =Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to public_customer_path(@customer.id)
+    else
+      render 'edit'
+    end
+  end
+  
+  def withdraw
+    @customer = Customer.find(params[:customer_id])
+    if @customer.update(is_active: false)
+      reset_session
+    end
+    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。" 
+    redirect_to root_path
+  end
+  
+    # 退会の仕方について
+  def unsubscribe
+    @customer = current_customer.id
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code,
+                                     :address, :telephone_number, :is_active)
+  end
 end
