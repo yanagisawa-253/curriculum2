3ab0c23fc3faba71b24a14f65d1e37d5eaadb38d
yanagisawa-253 <anaguma253@gmail.com>
2022-02-09 04:32:48 +0000

    [add]address.model追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_021813) do
+ActiveRecord::Schema.define(version: 2022_02_09_043143) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
